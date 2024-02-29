a3e48d6ebcf329d704e1695094f806bfc4dd93ef
yazca <yazcaamalfranco@gmail.com>
2024-02-29 10:27:10 -0600

    Se imprime nombre de usuario

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -5,6 +5,8 @@ public class EjemploGit {
 		// TODO Auto-generated method stub
 		System.out.println("Hola alumnos Git");
 		String nombre = "Yazmin Caamal";
+		System.out.println(nombre);
+		
 
 	}
 
